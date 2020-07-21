59f2663110c1b531700ce79249ade5f6d04b8585
Patrick Furtak <patrick@patrickfurtak.com>
2020-07-21 14:04:45 -0400

    Created Navbar component with conditional rendering

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import AuthState from './context/auth/AuthState';
 import setAuthToken from '../src/utils/setAuthToken';
 import AlertState from './context/alert/AlertState';
 import Alerts from './components/layout/Alerts';
+import Navbar from './components/layout/Navbar';
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -13,9 +15,14 @@ const App = () => {
   return (
     <AuthState>
       <AlertState>
-        <div className='container'>
-          <Alerts />
-        </div>
+        <Router>
+          <Fragment>
+            <Navbar />
+            <div className='container'>
+              <Alerts />
+            </div>
+          </Fragment>
+        </Router>
       </AlertState>
     </AuthState>
   );
