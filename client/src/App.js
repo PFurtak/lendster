83c3459ee08cc763c425ace4b5647d3fe2fb4bf9
Patrick Furtak <patrick@patrickfurtak.com>
2020-07-22 19:10:50 -0400

    Wrapped application with item state provider

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import AuthState from './context/auth/AuthState';
+import ItemState from './context/items/ItemState';
 import setAuthToken from '../src/utils/setAuthToken';
 import AlertState from './context/alert/AlertState';
 import Alerts from './components/layout/Alerts';
@@ -19,22 +20,24 @@ if (localStorage.token) {
 const App = () => {
   return (
     <AuthState>
-      <AlertState>
-        <Router>
-          <Fragment>
-            <Navbar />
-            <div className='container'>
-              <Alerts />
-              <Switch>
-                <PrivateRoute exact path='/' component={Home} />
-                <Route exact path='/about' component={About} />
-                <Route exact path='/register' component={Register} />
-                <Route exact path='/login' component={Login} />
-              </Switch>
-            </div>
-          </Fragment>
-        </Router>
-      </AlertState>
+      <ItemState>
+        <AlertState>
+          <Router>
+            <Fragment>
+              <Navbar />
+              <div className='container'>
+                <Alerts />
+                <Switch>
+                  <PrivateRoute exact path='/' component={Home} />
+                  <Route exact path='/about' component={About} />
+                  <Route exact path='/register' component={Register} />
+                  <Route exact path='/login' component={Login} />
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
+        </AlertState>
+      </ItemState>
     </AuthState>
   );
 };
