95a0332963acc6073ca979765f28c64053d5c8dc
Patrick Furtak <patrick@patrickfurtak.com>
2020-07-23 14:07:02 -0400

    Fixed multiple typos in imports, added dev scripts, added proxy setting in client package.json

--- client/src/context/types.js
+++ client/src/context/types.js
@@ -19,5 +19,5 @@ export const AUTH_ERROR = 'AUTH_ERROR';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAIL = 'LOGIN_FAIL';
 export const LOGOUT = 'LOGOUT';
-export const CONTACT_ERROR = 'CONTACT_ERROR';
+export const ITEM_ERROR = 'CONTACT_ERROR';
 export const CLEAR_ERRORS = 'CLEAR_ERRORS';
